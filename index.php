e13a66408341fbe39ceed3d3957f9f0fb81dc3f2
MohamedBoudaa <60492162+MohamedBoudaa@users.noreply.github.com>
2020-03-27 23:59:39 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>Imeqsan Band</title>
+        <link rel="stylesheet" href="styles.css?version=55">
+        <link href="https://fonts.googleapis.com/css?family=Dosis|Liu+Jian+Mao+Cao|Montserrat|Pacifico&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+
+    <body style="background-color: #f4f3f3;margin: 0;padding: 0;">
+        <div class="container1">
+
+            <div class="topbar">
+                <img class="logo2" src="images/logo2.jpg">
+            </div> 
+            <img class="logo" src="images/logo.jpg">
+
+        </div>
+        <div class="container2">
+            <div class="container3">
+                <!--                <hr class="hr1"color="#1abb9c" width="60%" >-->
+                <h4>Welcome to</h4>
+                <h2>Imeqsan Band<br/>Official Website</h2>
+            </div>
+            <div class="container4">
+                <hr class="hr1"color="#1abb9c" width="60%" >
+                <h4>About us</h4>
+                <h2>Here is a section about<br/>the band, debut and goals...</h2>
+            </div>
+            <div class="container5">
+                <hr class="hr1"color="#1abb9c" width="60%" >
+                <h4>Note :</h4>
+                <h2>Please note that this site is under construction,<br/>
+                    any constructive ideas or remarks will be welcomed.
+                    <br/>Please contact <a href="">med.boudaa@gmail.com</a></h2>
+            </div>
+        </div>
+
+    </div>
+    <div class="footer">
+        <p>© 2020 <a href="">Imeqsan Band</a>. All rights reserved.</p>
+    </div>
+</body>    
+
+</html>
+
+
